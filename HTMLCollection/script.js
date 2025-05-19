01d75b177ecf3882c9d0672926535711397665e6
Chebbi <said.chebbi41@gmail.com>
2025-05-19 13:54:54 +0200

    Ended HTMLCollection Exercice

---
+++ HTMLCollection/script.js
@@ -0,0 +1,13 @@
+// fichier script.js
+const bearsPictures = [
+    "https://placebear.com/500/279",
+    "https://placebear.com/500/280",
+    "https://placebear.com/500/300",
+    "https://placebear.com/500/302",
+    "https://placebear.com/500/305",
+];
+// À toi de jouer ici 👇
+const allImages = document.querySelectorAll('img');
+for (let i= 0; i < allImages.length; i++) {
+    allImages[i].src = bearsPictures[i];
+}
